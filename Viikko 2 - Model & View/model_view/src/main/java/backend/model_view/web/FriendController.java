b58f3d78d6c4782eaab88dfa3104bffea9fe0237
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-01-25 13:34:53 +0200

    viikko 2 tehtävä 4 tehty

--- Viikko 2 - Model & View/model_view/src/main/java/backend/model_view/web/FriendController.java
+++ Viikko 2 - Model & View/model_view/src/main/java/backend/model_view/web/FriendController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import backend.model_view.domain.Friend;
 
 import java.util.ArrayList;
@@ -12,14 +11,13 @@ import java.util.List;
 
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FriendController {
 
     private List<Friend> friends = new ArrayList<>();
 
- 
+    // tehtävä 4, lisää kaveri samalla sivulla
     @GetMapping("/friends")
     public String showFriends(Model model) {
         model.addAttribute("friend", new Friend());
@@ -35,5 +33,13 @@ public class FriendController {
         }
         friends.add(friend);
         return "redirect:/friends";
+    }
+
+    // tehtävä 3, lisää kaveri linkin takaa
+    @GetMapping("/add")
+    public String showFriendsAdd(Model model) {
+        model.addAttribute("friend", new Friend());
+        model.addAttribute("friends", friends);
+        return "add";
     }
 }
