d3012545d2f6b412efb39d8578d830ada0692c5d
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-01-25 13:39:54 +0200

    Luotu Bookstore loppuprojekti

---
+++ Bookstore/jarnobookstore/src/main/java/home/jarnobookstore/JarnobookstoreApplication.java
@@ -0,0 +1,13 @@
+package home.jarnobookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarnobookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarnobookstoreApplication.class, args);
+	}
+
+}
---
+++ Bookstore/jarnobookstore/src/test/java/home/jarnobookstore/JarnobookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package home.jarnobookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarnobookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
