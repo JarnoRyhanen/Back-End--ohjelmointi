5a728295b3764bb67b9e032e62a20b715b8ac940
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-02-27 16:03:38 +0200

    Added spring security. Homework part 1 finished.

--- Bookstore/jarnobookstore/src/main/java/home/jarnobookstore/web/BookController.java
+++ Bookstore/jarnobookstore/src/main/java/home/jarnobookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,6 +59,7 @@ public class BookController {
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteBook(@PathVariable("id") Long id, Model model) {
         bookRepository.deleteById(id);
         return "redirect:../booklist";
@@ -81,5 +83,11 @@ public class BookController {
     public @ResponseBody Optional<Book> findBookByIdRest(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId);
     }
+
+    @RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+    
 
 }
