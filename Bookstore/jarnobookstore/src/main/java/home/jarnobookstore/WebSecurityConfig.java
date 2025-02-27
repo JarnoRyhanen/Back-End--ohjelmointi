cac4366d2c64e076b52bd8570f83bcff876e332d
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-02-27 18:03:28 +0200

    Security homework part 2 done

--- Bookstore/jarnobookstore/src/main/java/home/jarnobookstore/WebSecurityConfig.java
+++ Bookstore/jarnobookstore/src/main/java/home/jarnobookstore/WebSecurityConfig.java
@@ -1,18 +1,14 @@
 package home.jarnobookstore;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,13 +16,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
+        private UserDetailsService userDetailsService;
+
+        public WebSecurityConfig(UserDetailsService userDetailsService) {
+                this.userDetailsService = userDetailsService;
+        }
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        }
+
         @Bean
         public SecurityFilterChain configure(HttpSecurity http) throws Exception {
                 http
                                 .authorizeHttpRequests(authorize -> authorize
                                                 .anyRequest().authenticated())
                                 .formLogin(formlogin -> formlogin
-                                                .loginPage("/login")
+                                               // .loginPage("/login")
                                                 .defaultSuccessUrl("/booklist", true)
                                                 .permitAll())
                                 .logout(logout -> logout
@@ -34,25 +41,4 @@ public class WebSecurityConfig {
                 ;
                 return http.build();
         }
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-                UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("user")
-                                .password("password")
-                                .roles("USER")
-                                .build();
-
-                UserDetails admin = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("adminpassword")
-                                .roles("ADMIN")
-                                .build();
-
-                List<UserDetails> users = new ArrayList<UserDetails>();
-                users.add(user);
-                users.add(admin);
-                return new InMemoryUserDetailsManager(users);
-        }
-
 }
--- Bookstore/jarnobookstore/src/main/resources/schema.sql
+++ Bookstore/jarnobookstore/src/main/resources/schema.sql
@@ -20,3 +20,12 @@ CREATE TABLE IF NOT EXISTS book (
     PRIMARY KEY (book_id),
     FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
